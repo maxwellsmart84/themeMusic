6aa4bb1f665cc7e42b48112229ce003788289266
Maxwell Krause <maxwell.l.krause@gmail.com>
2018-07-19 20:28:55 -0400

    hooking it all up

--- src/services/firebase.service.js
+++ src/services/firebase.service.js
@@ -9,17 +9,36 @@ admin.initializeApp({
 
 const db = admin.database();
 const spotifyTokenRef = db.ref().child('spotify-token');
+const usersRef = db.ref().child('users');
 
 export async function postToken(token) {
   try {
-    const tokenData = {
-      token,
-    };
-    await spotifyTokenRef.push({
-      tokenData,
-    });
+    const tokenData = { token };
+    await spotifyTokenRef.push({ tokenData });
     return true;
   } catch (err) {
     throw new Error(err);
   }
 }
+
+export function getUser(id) {
+  console.log('Figure out Get by id');
+  return id;
+}
+
+export function patchUser(id, data) {
+  console.log('---ARGS PATCH USER---', id, data);
+  return id;
+}
+
+
+export async function postUser(userData) {
+  try {
+    const user = await usersRef.push({ userData });
+    return user;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+}
+
